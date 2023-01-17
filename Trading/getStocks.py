c0eae1e4fb4eb0809e1bdade6a83b85335ded1fa
Isaac Abell <83926340+Isaac-Abell@users.noreply.github.com>
2023-01-17 16:20:08 -0500

    Update getStocks.py

--- Trading/getStocks.py
+++ Trading/getStocks.py
@@ -3,21 +3,19 @@ import json
 import requests
 import privateInfo
 
-
 def getStocks(location):
+    """gets JSON file at location and converts the data into a list"""
+
     if os.path.exists(location):
         with open(location) as stockJson:
-            data = json.loads(stockJson.read()) #converts the JSON into usable data
-
-        #takes data, trims all unwanted elements and gets a list of stocks to trade 
-        stock = data
+            data = json.loads(stockJson.read())
+        stock = data 
         stocks = stock.split()
 
         stocksArray=[]
         for i in stocks:
             stocksArray.append(str(i.replace('"', "")))
 
-        #deletes json file to prevent repeats
         os.remove(location)
         print(location + "deleted")
 
@@ -27,34 +25,33 @@ def getStocks(location):
         print("The file does not exist")
         return []
 
+key = ''
 
 def getQuotes(**kwargs):
-
+    """gets a stocks data as a dictionary"""
     url = 'https://api.tdameritrade.com/v1/marketdata/quotes'
-    param = {}
+    params = {}
     key = privateInfo.api_key
 
-    param.update({'apiKey': key})
+    params.update({'apikey': key})
     symbol_list = []
 
     for symbol in kwargs.get('symbol'):
         symbol_list.append(symbol)
     
-    param.update({'symbol': symbol_list})
+    params.update({'symbol': symbol_list})
 
-    return requests.get(url, params = param).json()
+    return requests.get(url, params = params).json()
 
 
 def getPricesAndStocks(location):
-
+    """makes a list of lists that contains stock price and stock symbol"""
     stockData = getStocks(location)
-    data = getQuotes(stockData)
+    data = getQuotes(symbol = stockData)
     
-    returnData =[]
+    returnData = []
 
     for stock in stockData:
         returnData.append([stock, data[stock]["lastPrice"]])
     
     return returnData
-
-buyData = getPricesAndStocks("stocksToBuy.json")
